0b8f3e1bc2b2337c9854154f311ec83b2a7a819f
Offych <off@tut.by>
2019-10-18 09:57:16 +0300

    foreach multidimensional arrays

--- foreach/foreach.js
+++ foreach/foreach.js
@@ -14,5 +14,38 @@ dwarves.forEach(function (item, index, array) {
         item = item.toLowerCase();
     }
 
-    console.log(index, item);
+    //console.log(index, item);
 })
+
+let grades = [12, 12, 32, 43, 42, 23];
+
+/*for (let i = 0; i < grades.length; i++) {
+    console.log(grades[i]);
+} */
+/* 
+grades.forEach(function (item, i, array) {
+    console.log(i, item);
+}) */
+
+let grades1 = [
+    [12, 13, 32, 43, 42, 23],
+    [12, 43, 14, 43, 33, 23, 44, 77, 88,],
+    [12, 18, 55, 43, 42, 19]
+];
+
+/* for (let i = 0; i < grades1.length; i++) {
+    for (let k = 0; k < grades1[i].length; k++) {
+        console.log(grades1[i][k]);
+    }
+    console.log("~~~~~~~")
+} */
+
+grades1.forEach(function (row) {
+    row.forEach(function (col) {
+        console.log(col)
+    })
+    console.log("----")
+}
+)
+
+
